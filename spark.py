3f96b70dec4a0d32b0c1b12c86b3dc46e082ac2e
JooHyun Kim <kjh36102@gmail.com>
2023-01-08 00:33:07 +0900

    Stablize TUI Select feature and organize basic form of spark.py

--- spark.py
+++ spark.py
@@ -1,59 +1,72 @@
-MAX_WIDTH = 50
-SEP_BAR = '-' * MAX_WIDTH
+import sys
+sys.path.append('./spark/')
 
-def print_at(str, idx):
-    print(' ' * idx + str)
+from Selector import Selector
 
-def index():
-    print(SEP_BAR)
-    print('* WELCOME TO SPARK MANAGER *'.center(MAX_WIDTH))
-    print()
+def main_menu():
+    idx, _ = Selector(prompt='Welcom to Spark!', item_list=[
+        'create new post',
+        'synchronize post with FTP server',
+        'config post',
+        'config category',
+        'convert image URL',
+        'revert image URL',
+        'config FTP info',
+        'config CSS style',
+        'initialize blog',
+    ]).run()
 
-    #print index
-    print('[Please type number of action]')
-    print('1. Config Post          | 5. Initialize blog')
-    print('2. Config Category      | 6. Config Img Base URL')
-    print('3. Convert Img URL      | 7. Config Disqus')
-    print('4. Revert Img URL       | 8. Config CSS Style')
-    print()
-    return input('What would you do? >> ')
+    return idx
+
+def create_new_post():
+    print('create_new_post')
+    pass
+
+def synchronize_post():
+    print('synchronize_post')
+    pass
 
 def config_post():
+    print('config_post')
     pass
 
 def config_category():
+    print('config_category')
     pass
 
-def convert_img_url():
+def convert_image_url():
+    print('convert_image_url')
     pass
 
-def revert_img_url():
+def revert_image_url():
+    print('revert_image_url')
     pass
 
-def initialize_blog():
-    pass
-
-def config_img_base_url():
-    pass
-
-def config_disqus():
+def config_ftp_info():
+    print('config_ftp_info')
     pass
 
 def config_css_style():
+    print('config_css_style')
+    pass
+
+def initialize_blog():
+    print('initialize blog')
     pass
 
 # main -------------------------------------------
 try:
-    idx = index()
+    idx = main_menu() + 1
 
-    if idx == '1': config_post()
-    elif idx == '2': config_category()
-    elif idx == '3': convert_img_url()
-    elif idx == '4': revert_img_url()
-    elif idx == '5': initialize_blog()
-    elif idx == '6': config_img_base_url()
-    elif idx == '7': config_disqus()
-    elif idx == '8': config_css_style()
+    if idx == 1: create_new_post()
+    elif idx == 2: synchronize_post()
+    elif idx == 3: config_post()
+    elif idx == 4: config_category()
+    elif idx == 5: convert_image_url()
+    elif idx == 6: revert_image_url()
+    elif idx == 7: config_ftp_info()
+    elif idx == 8: config_css_style()
+    elif idx == 9: initialize_blog()
     else: raise IndexError('Out of selection range')
 
 except Exception as e:
